92fe6410ca63d67f67e7fb78c28545e86908b946
Sarah Sharp <sharp@otter.technology>
2017-08-02 14:48:38 -0700

    Initial commit of wagtail django starting site.
    Signed-off-by: Sarah Sharp <sharp@otter.technology>

---
+++ outreachyhome/settings/dev.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import, unicode_literals
+
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'k^)qcm)yhze23d2pk!zx7#ip3%)yxj!9&hpe1a3r_4#1gr_9&h'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
