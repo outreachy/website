73db7ef089ab21eaa68abddd09bfefc43d7a79f8
Sarah Sharp <sharp@otter.technology>
2017-08-02 15:11:47 -0700

    Extend HomePage model to have a rich-text field.
    Signed-off-by: Sarah Sharp <sharp@otter.technology>

--- home/models.py
+++ home/models.py
@@ -3,7 +3,12 @@ from __future__ import absolute_import, unicode_literals
 from django.db import models
 
 from wagtail.wagtailcore.models import Page
-
+from wagtail.wagtailcore.fields import RichTextField
+from wagtail.wagtailadmin.edit_handlers import FieldPanel
 
 class HomePage(Page):
-    pass
+    body = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('body', classname="full"),
+    ]
