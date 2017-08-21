7f3564c0185279cb82fe9b0953e365a4fa07fe69
Sarah Sharp <sharp@otter.technology>
2017-08-21 15:35:39 -0700

    Mentor confirmation view and form.
    Still need to tie in the base64 unique mentor ID assigned when the
    coordinator added the mentor.
    
    Signed-off-by: Sarah Sharp <sharp@otter.technology>

--- outreachyhome/urls.py
+++ outreachyhome/urls.py
@@ -9,6 +9,7 @@ from wagtail.wagtailcore import urls as wagtail_urls
 from wagtail.wagtaildocs import urls as wagtaildocs_urls
 
 from search import views as search_views
+from home import views as home_views
 
 urlpatterns = [
     url(r'^django-admin/', include(admin.site.urls)),
@@ -17,6 +18,7 @@ urlpatterns = [
     url(r'^documents/', include(wagtaildocs_urls)),
 
     url(r'^search/$', search_views.search, name='search'),
+    url(r'^mentor-confirm/$', home_views.MentorConfirmationView.as_view(), name='mentor-confirm'),
 
     # For anything not caught by a more specific rule above, hand over to
     # Wagtail's page serving mechanism. This should be the last pattern in
