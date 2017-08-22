cc524d5c6fb835b18e4a020ab86bdf5b4555e9be
Sarah Sharp <sharp@otter.technology>
2017-08-21 19:05:25 -0700

    Fix BASE_URL
    Signed-off-by: Sarah Sharp <sharp@otter.technology>

--- outreachyhome/settings/base.py
+++ outreachyhome/settings/base.py
@@ -145,7 +145,7 @@ WAGTAIL_SITE_NAME = "outreachyhome"
 
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
-BASE_URL = 'http://example.com'
+BASE_URL = 'https://www.outreachy.org'
 REGISTRATION_OPEN = True
 LOGIN_REDIRECT_URL='/'
 LOGIN_URL='/login/'
