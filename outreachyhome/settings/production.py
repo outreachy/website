6ef1d5999bbec9237a3ced69a2ceb2cbb47058f4
Sarah Sharp <sharp@otter.technology>
2017-12-05 13:57:38 -0800

    Add support for Django to use email.
    On the Outreachy django server, the environment variables are set up
    with dokku to use the Outreachy mail server and a separate Django
    username and password.

--- outreachyhome/settings/production.py
+++ outreachyhome/settings/production.py
@@ -9,6 +9,13 @@ DEBUG = False
 ALLOWED_HOSTS = [ '162.242.218.160', '.outreachy.org' ]
 
 SECRET_KEY = os.environ['SECRET_KEY']
+EMAIL_HOST = os.environ.get('EMAIL_HOST', 'localhost')
+# Environment variables are strings, so we need to convert to an integer
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 25))
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
+# Environment variables are strings, so we need to convert to an bool
+EMAIL_USE_SSL = bool(os.environ.get('EMAIL_USE_SSL', False))
 
 try:
     from .local import *
