1adc9c04391c263674fe45f652627f802227d87c
Sage Sharp <sharp@otter.technology>
2017-12-29 12:02:03 -0800

    Fix URL order for community create/edit/delete pages
    The add needs to be before the community read-only page regex.

--- home/urls.py
+++ home/urls.py
@@ -6,10 +6,10 @@ from .views import CommunityCreate, CommunityUpdate, CommunityDelete
 from django.conf.urls import url
 
 urlpatterns = [
-    url(r'^communities/cfp/(?P<slug>[^/]+)/$', community_read_only_view, name='community-read-only'),
-    url(r'^(?P<round_slug>[^/]+)/communities/(?P<slug>[^/]+)/$', community_landing_view, name='community-landing'),
-    url(r'^communities/cfp/$', community_cfp_view, name='community-cfp'),
     url(r'^communities/cfp/add/$', CommunityCreate.as_view(), name='community-add'),
     url(r'^communities/cfp/(?P<slug>[^/]+)/edit/$', CommunityUpdate.as_view(), name='community-update'),
     url(r'^communities/cfp/(?P<slug>[^/]+)/delete/$', CommunityDelete.as_view(), name='community-delete'),
+    url(r'^communities/cfp/(?P<slug>[^/]+)/$', community_read_only_view, name='community-read-only'),
+    url(r'^(?P<round_slug>[^/]+)/communities/(?P<slug>[^/]+)/$', community_landing_view, name='community-landing'),
+    url(r'^communities/cfp/$', community_cfp_view, name='community-cfp'),
 ]
