df4df46a17404079f96d09eb91bbcc3e621cdac1
Jamey Sharp <jamey@minilop.net>
2018-01-13 16:25:38 -0800

    Link dashboard from navbar, with badge for pending requests.

--- home/context_processors.py
+++ home/context_processors.py
@@ -1,4 +1,19 @@
 from wagtail.wagtailcore.models import Page
+from . import models
 
-def headerpages(request):
-    return { 'header_pages': Page.objects.live().in_menu() }
+def header(request):
+    dashboard_models = (
+            models.CoordinatorApproval,
+            models.Participation,
+            models.Project,
+            models.MentorApproval,
+            )
+
+    pending_approvals = sum(
+            model.objects_for_dashboard(request.user).filter(approval_status=models.ApprovalStatus.PENDING).count()
+            for model in dashboard_models)
+
+    return {
+            'header_pages': Page.objects.live().in_menu(),
+            'pending_approvals': pending_approvals,
+            }
--- outreachyhome/settings/base.py
+++ outreachyhome/settings/base.py
@@ -90,7 +90,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'home.context_processors.headerpages',
+                'home.context_processors.header',
             ],
         },
     },
