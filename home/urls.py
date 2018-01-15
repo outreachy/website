6b64d3e665a50d61451b9d39a37045938015efd7
Jamey Sharp <jamey@minilop.net>
2018-01-15 00:27:43 -0800

    Split URL patterns into groups by common prefixes.
    This reduces duplication in the URL patterns so they are marginally
    easier to read. Perhaps more importantly, this makes it easier to verify
    that the regex for one URL pattern can't accidentally match another one.

--- home/urls.py
+++ home/urls.py
@@ -1,21 +1,31 @@
 from . import views
 
-from django.conf.urls import url
+from django.conf.urls import include, url
+
+# These views all take both a community_slug and a project_slug.
+project_cfp_patterns = [
+    url(r'^mentor/preview/(?P<username>[^/]+)/$', views.MentorApprovalPreview.as_view(), name='mentorapproval-preview'),
+    url(r'^mentor/(?P<action>[^/]+)/(?:(?P<username>[^/]+)/)?$', views.MentorApprovalAction.as_view(), name='mentorapproval-action'),
+    url(r'^skills/$', views.ProjectSkillsEditPage.as_view(), name='project-skills-edit'),
+    url(r'^channels/$', views.CommunicationChannelsEditPage.as_view(), name='communication-channels-edit'),
+    url(r'^$', views.project_read_only_view, name='project-read-only'),
+]
+
+# These views all take a community_slug.
+community_cfp_patterns = [
+    url(r'^edit/$', views.CommunityUpdate.as_view(), name='community-update'),
+    url(r'^notify/$', views.CommunityNotificationUpdate.as_view(), name='notify-me'),
+    url(r'^coordinator/preview/(?P<username>[^/]+)/$', views.CoordinatorApprovalPreview.as_view(), name='coordinatorapproval-preview'),
+    url(r'^coordinator/(?P<action>[^/]+)/(?:(?P<username>[^/]+)/)?$', views.CoordinatorApprovalAction.as_view(), name='coordinatorapproval-action'),
+    url(r'^project/(?P<project_slug>[^/]+)/', include(project_cfp_patterns)),
+    url(r'^(?P<action>[^/]+)-project/(?:(?P<project_slug>[^/]+)/)?$', views.ProjectAction.as_view(), name='project-action'),
+    url(r'^(?P<action>[^/]+)/$', views.ParticipationAction.as_view(), name='participation-action'),
+    url(r'^$', views.community_read_only_view, name='community-read-only'),
+]
 
 urlpatterns = [
     url(r'^communities/cfp/add/$', views.CommunityCreate.as_view(), name='community-add'),
-    url(r'^communities/cfp/(?P<community_slug>[^/]+)/edit/$', views.CommunityUpdate.as_view(), name='community-update'),
-    url(r'^communities/cfp/(?P<community_slug>[^/]+)/notify/$', views.CommunityNotificationUpdate.as_view(), name='notify-me'),
-    url(r'^communities/cfp/(?P<community_slug>[^/]+)/coordinator/preview/(?P<username>[^/]+)/$', views.CoordinatorApprovalPreview.as_view(), name='coordinatorapproval-preview'),
-    url(r'^communities/cfp/(?P<community_slug>[^/]+)/coordinator/(?P<action>[^/]+)/(?:(?P<username>[^/]+)/)?$', views.CoordinatorApprovalAction.as_view(), name='coordinatorapproval-action'),
-    url(r'^communities/cfp/(?P<community_slug>[^/]+)/project/(?P<project_slug>[^/]+)/mentor/preview/(?P<username>[^/]+)/$', views.MentorApprovalPreview.as_view(), name='mentorapproval-preview'),
-    url(r'^communities/cfp/(?P<community_slug>[^/]+)/project/(?P<project_slug>[^/]+)/mentor/(?P<action>[^/]+)/(?:(?P<username>[^/]+)/)?$', views.MentorApprovalAction.as_view(), name='mentorapproval-action'),
-    url(r'^communities/cfp/(?P<community_slug>[^/]+)/project/(?P<project_slug>[^/]+)/skills/$', views.ProjectSkillsEditPage.as_view(), name='project-skills-edit'),
-    url(r'^communities/cfp/(?P<community_slug>[^/]+)/project/(?P<project_slug>[^/]+)/channels/$', views.CommunicationChannelsEditPage.as_view(), name='communication-channels-edit'),
-    url(r'^communities/cfp/(?P<community_slug>[^/]+)/project/(?P<project_slug>[^/]+)/$', views.project_read_only_view, name='project-read-only'),
-    url(r'^communities/cfp/(?P<community_slug>[^/]+)/(?P<action>[^/]+)-project/(?:(?P<project_slug>[^/]+)/)?$', views.ProjectAction.as_view(), name='project-action'),
-    url(r'^communities/cfp/(?P<community_slug>[^/]+)/(?P<action>[^/]+)/$', views.ParticipationAction.as_view(), name='participation-action'),
-    url(r'^communities/cfp/(?P<community_slug>[^/]+)/$', views.community_read_only_view, name='community-read-only'),
+    url(r'^communities/cfp/(?P<community_slug>[^/]+)/', include(community_cfp_patterns)),
     url(r'^(?P<round_slug>[^/]+)/communities/(?P<slug>[^/]+)/$', views.community_landing_view, name='community-landing'),
     url(r'^communities/cfp/$', views.community_cfp_view, name='community-cfp'),
     url(r'^dashboard/$', views.dashboard, name='dashboard'),
