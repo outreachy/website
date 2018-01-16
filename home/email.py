a56d311c8af1a166918ae5be7f793a6d86ce8314
Jamey Sharp <jamey@minilop.net>
2018-01-15 22:45:55 -0800

    Move email subject lines into templates.
    Calling .format just clutters the code here.
    
    I don't want to do the same thing for headers that contain email
    addresses because quoting email addresses correctly is complicated and
    best left to Python code, not templates.

--- home/email.py
+++ home/email.py
@@ -6,8 +6,9 @@ organizers = Address("Outreachy Organizers", "organizers", "outreachy.org")
 
 def send_template_mail(template, context, request=None, **kwargs):
     message = render_to_string(template, context, request=request, using='plaintext')
+    subject, body = message.split('\n', 1)
     kwargs.setdefault('from_email', organizers)
-    send_mail(message=message.strip(), **kwargs)
+    send_mail(message=body.strip(), subject=subject.strip(), **kwargs)
 
 def participation_pending(participation, request):
     send_template_mail('home/email/community-signup.txt', {
@@ -16,7 +17,6 @@ def participation_pending(participation, request):
         'participation_info': participation,
         },
         request=request,
-        subject='Approve community participation - {name}'.format(name=participation.community.name),
         recipient_list=[organizers])
 
 def notify_mentor(participation, notification, request):
@@ -26,7 +26,6 @@ def notify_mentor(participation, notification, request):
         'current_round': participation.participating_round,
         },
         request=request,
-        subject='Mentor for {name} in Outreachy'.format(name=participation.community.name),
         recipient_list=[notification.comrade.email_address()])
 
 def project_pending(project, request):
@@ -36,7 +35,6 @@ def project_pending(project, request):
         'project': project,
         },
         request=request,
-        subject='Approve Outreachy intern project proposal for {name}'.format(name=community.name),
         recipient_list=community.get_coordinator_email_list())
 
 def project_nonfree_warning(project, request):
@@ -46,7 +44,6 @@ def project_nonfree_warning(project, request):
         'project': project,
         },
         request=request,
-        subject='Approve Outreachy intern project proposal for {name}'.format(name=community.name),
         recipient_list=[organizers])
 
 def mentorapproval_pending(mentorapproval, request):
@@ -57,17 +54,14 @@ def mentorapproval_pending(mentorapproval, request):
         'mentorapproval': mentorapproval,
         },
         request=request,
-        subject='Approve Outreachy mentor for {name}'.format(name=community.name),
         recipient_list=community.get_coordinator_email_list())
 
 def mentorapproval_approved(mentorapproval, request):
-    community = mentorapproval.project.project_round.community
     send_template_mail('home/email/mentor-approved.txt', {
-        'community': community,
+        'community': mentorapproval.project.project_round.community,
         'project': mentorapproval.project,
         },
         request=request,
-        subject='Approved as Outreachy mentor for {name}'.format(name=community.name),
         recipient_list=[mentorapproval.mentor.email_address()])
 
 def mentor_list_subscribe(mentor, request):
@@ -86,5 +80,4 @@ def mentor_list_subscribe(mentor, request):
                 domain=request.get_host()),
             addr_spec=mentor.account.email
             ),
-        subject='Subscribe {name}'.format(name=mentor.public_name),
         recipient_list=['mentors-join@lists.outreachy.org'])
