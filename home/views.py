outreachy/website

commit 6eefd7cdd92d3ef183c63083dba7fcde8723d26b

Add get_action_url helpers to models.

home/views.py
@@ -314,11 +314,12 @@ class CommunityCreate(LoginRequiredMixin, ComradeRequiredMixin, CreateView):
                 community=self.object,
                 approval_status=ApprovalStatus.APPROVED)
 
-        # When a new community is created, immediately redirect the coordinator
-        # to gather information about their participation in this round
-        return redirect('participation-action',
-                action='submit',
-                community_slug=self.object.slug)
+        # When a new community is created, immediately redirect the
+        # coordinator to gather information about their participation in
+        # this round. The Participation object doesn't have to be saved
+        # to the database yet; that'll happen when they submit it in the
+        # next step.
+        return Participation(community=self.object).get_action_url('submit')
 
 class CommunityUpdate(LoginRequiredMixin, UpdateView):
     model = Community
@@ -544,15 +545,11 @@ class ProjectAction(ApprovalStatusAction):
     def get_success_url(self):
         if not self.kwargs.get('project_slug'):
             # If this is a new Project, associate an approved mentor with it
-            MentorApproval.objects.create(
+            mentor = MentorApproval.objects.create(
                     mentor=self.request.user.comrade,
                     project=self.object,
                     approval_status=ApprovalStatus.APPROVED)
-            return reverse('mentorapproval-action', kwargs={
-                'community_slug': self.object.project_round.community.slug,
-                'project_slug': self.object.slug,
-                'action': 'submit',
-                })
+            return mentor.get_action_url('submit', self.request.user)
         elif self.kwargs['action'] == 'submit':
             return reverse('project-skills-edit', kwargs={
                 'community_slug': self.object.project_round.community.slug,

home/models.py
@@ -445,6 +445,12 @@ class Participation(ApprovalStatus):
     def get_preview_url(self):
         return self.community.get_preview_url()
 
+    def get_action_url(self, action):
+        return reverse('participation-action', kwargs={
+            'community_slug': self.community.slug,
+            'action': action,
+            })
+
     def is_approver(self, user):
         return user.is_staff
 
@@ -572,6 +578,13 @@ class Project(ApprovalStatus):
 
     def get_preview_url(self):
         return reverse('project-read-only', kwargs={'community_slug': self.project_round.community.slug, 'project_slug': self.slug})
+
+    def get_action_url(self, action):
+        return reverse('project-action', kwargs={
+            'community_slug': self.project_round.community.slug,
+            'project_slug': self.slug,
+            'action': action,
+            })
 
     def is_approver(self, user):
         return self.project_round.community.is_coordinator(user)
@@ -760,6 +773,16 @@ class MentorApproval(ApprovalStatus):
             'username': self.mentor.account.username,
             })
 
+    def get_action_url(self, action, current_user=None):
+        kwargs = {
+                'community_slug': self.project.project_round.community.slug,
+                'project_slug': self.project.slug,
+                'action': action,
+                }
+        if self.mentor.account != current_user:
+            kwargs['username'] = self.mentor.account.username
+        return reverse('mentorapproval-action', kwargs=kwargs)
+
     def is_approver(self, user):
         return self.project.project_round.community.is_coordinator(user)
 
@@ -863,6 +886,15 @@ class CoordinatorApproval(ApprovalStatus):
             'community_slug': self.community.slug,
             'username': self.coordinator.account.username,
             })
+
+    def get_action_url(self, action, current_user=None):
+        kwargs = {
+                'community_slug': self.community.slug,
+                'action': action,
+                }
+        if self.coordinator.account != current_user:
+            kwargs['username'] = self.coordinator.account.username
+        return reverse('coordinatorapproval-action', kwargs=kwargs)
 
     def is_approver(self, user):
         return user.is_staff or self.community.is_coordinator(user)

commit 8045575141d06cbadb23ff19835d2fcd50a2b20a

CommunityCreate.form_valid: Return response, not URL.
I broke this in commit 6eefd7cd.

home/views.py
@@ -321,7 +321,7 @@ class CommunityCreate(LoginRequiredMixin, ComradeRequiredMixin, CreateView):
         # this round. The Participation object doesn't have to be saved
         # to the database yet; that'll happen when they submit it in the
         # next step.
-        return Participation(community=self.object).get_action_url('submit')
+        return redirect(Participation(community=self.object).get_action_url('submit'))
 
 class CommunityUpdate(LoginRequiredMixin, UpdateView):
     model = Community