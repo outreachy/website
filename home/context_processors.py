82feea70f40f5795f13d6844044079b4905778c9
Jamey Sharp <jamey@minilop.net>
2018-03-18 22:45:53 -0700

    Remove unapproved objects from dashboard after deadline.
    Fixes #182
    
    However, the "X actions needed" number in the top navbar is now
    inaccurate and kind of annoying to fix, so we should come back to it
    when there aren't so many other things to do.

--- home/context_processors.py
+++ home/context_processors.py
@@ -3,6 +3,7 @@ from .models import ApprovalStatus, DASHBOARD_MODELS
 
 def header(request):
     if request.user.is_authenticated:
+        # TODO: don't count objects where the submission_and_approval_deadline has passed
         pending_approvals = sum(
                 model.objects_for_dashboard(request.user).filter(approval_status=ApprovalStatus.PENDING).distinct().count()
                 for model in DASHBOARD_MODELS)
