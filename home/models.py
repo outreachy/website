731e1da3c9d74e47b2c0b852c2b826be086865e2
Sage Sharp <sharp@otter.technology>
2018-09-19 18:55:30 -0400

    Change wording on bias/discrimination question.
    People keep saying that they haven't contributed to FOSS yet. Try to
    change the question a bit.

--- home/models.py
+++ home/models.py
@@ -2483,7 +2483,7 @@ class BarriersToParticipation(models.Model):
             help_text="Please provide specific examples. Outreachy organizers strongly encourage you to write your personal stories. We want you to know that we won't judge your writing style, grammar or spelling.")
 
     systematic_bias = models.TextField(
-            verbose_name='What systematic bias or discrimination have you faced while building your skills for contributing to free and open source software?',
+            verbose_name='What systematic bias or discrimination have you faced while building your skills?',
             help_text="<p>Contributing to free and open source software takes some skill. You may have already learned some basic skills through university or college classes, specialized schools, online classes, online resources, or with a mentor, friend, family member or co-worker.</p><p>In these settings, have you faced systematic bias or discrimination? Have you been discouraged from accessing these resources because of your identity or background?</p><p>Please provide specific examples and (optionally) statistics.</p><p>Outreachy Organizers strongly encourage you to write your personal stories. We want you to know that we won't judge your writing style, grammar or spelling.</p>")
 
     lacking_representation = models.TextField(
