49434e936bbaaf4ad022969e13e1f2f610c267ed
Sage Sharp <sharp@otter.technology>
2018-10-24 10:13:27 -0700

    Add anchor tags to all projects on /apply/project-selection/

--- home/models.py
+++ home/models.py
@@ -1636,6 +1636,9 @@ class Project(ApprovalStatus):
     def get_preview_url(self):
         return reverse('project-read-only', kwargs={'community_slug': self.project_round.community.slug, 'project_slug': self.slug})
 
+    def get_project_selection_url(self):
+        return reverse('project-selection') + '#' + self.project_round.community.slug + '-' + self.slug
+
     def get_landing_url(self):
         return reverse('community-landing', kwargs={'round_slug': self.project_round.participating_round.slug, 'slug': self.project_round.community.slug}) + '#' + self.slug
 
