ca1d20b258fc3014de69eac5f25ade5cdc5ff171
Jamey Sharp <jamey@minilop.net>
2018-12-13 15:53:01 -0800

    Add factories for fake InternSelections and dependencies

--- home/factories.py
+++ home/factories.py
@@ -91,7 +91,7 @@ class RoundPageFactory(PageFactory):
     But you can override which deadline you build the timeline from by setting,
     for example, ``start_from="internstarts"``; and you can set the date for
     that deadline with ``start_date``.
-    
+
     Finally, you can change the delay between adjacent deadlines: for example,
     if you want a shorter delay between ``orgreminder`` and ``landingdue``, you
     could set ``landingdue=datetime.timedelta(days=7)``.
@@ -133,7 +133,7 @@ class RoundPageFactory(PageFactory):
         """
         start = obj.start_date
         if obj.start_from != 'pingnew':
-            assert obj.start_from in round_dates
+            assert obj.start_from in round_dates, "unknown RoundPage field: " + repr(obj.start_from)
             for field_name in round_dates:
                 start -= getattr(obj, field_name)
                 if obj.start_from == field_name:
@@ -151,3 +151,117 @@ class RoundPageFactory(PageFactory):
             previous += kwargs[field_name]
             kwargs[field_name] = previous
         return kwargs
+
+class CommunityFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.Community
+
+    name = factory.Faker('word')
+    slug = factory.Faker('slug')
+    description = factory.Faker('paragraph')
+
+class ParticipationFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.Participation
+
+    community = factory.SubFactory(CommunityFactory)
+    participating_round = factory.SubFactory(RoundPageFactory)
+
+class ProjectFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.Project
+        django_get_or_create = ('slug', 'project_round')
+
+    project_round = factory.SubFactory(ParticipationFactory)
+    short_title = factory.Faker('sentence')
+    slug = factory.Faker('slug')
+
+    contribution_tasks = factory.Faker('paragraph')
+    deadline = models.Project.ONTIME
+
+class MentorApprovalFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.MentorApproval
+        django_get_or_create = ('mentor', 'project')
+
+    mentor = factory.SubFactory(ComradeFactory)
+    project = factory.SubFactory(ProjectFactory)
+
+    mentor_foss_contributions = factory.Faker('paragraph')
+    mentorship_style = factory.Faker('paragraph')
+
+class ApplicantApprovalFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.ApplicantApproval
+        django_get_or_create = ('applicant', 'application_round')
+
+    applicant = factory.SubFactory(ComradeFactory)
+    application_round = factory.SubFactory(RoundPageFactory)
+    submission_date = factory.Faker('past_date')
+    ip_address = factory.Faker('ipv4_public')
+
+class SignedContractFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.SignedContract
+
+    text = factory.Faker('text')
+    legal_name = factory.Faker('name')
+    ip_address = factory.Faker('ipv4_public')
+    date_signed = factory.Faker('past_date')
+
+class InternSelectionFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.InternSelection
+        django_get_or_create = ('applicant', 'project')
+
+    class Params:
+        round = factory.SubFactory(RoundPageFactory, start_from='internannounce')
+
+        signed = factory.Trait(
+            intern_contract=factory.SubFactory(SignedContractFactory)
+        )
+
+        active = factory.Trait(
+            # checked by home.views.intern_in_good_standing()
+            project__approval_status=models.ApprovalStatus.APPROVED,
+            project__project_round__approval_status=models.ApprovalStatus.APPROVED,
+            organizer_approved=True,
+            in_good_standing=True,
+
+            # but these also ought to be set
+            signed=True,
+            #mentors=1, # the post_generation function is not called if this is set
+        )
+
+    applicant = factory.SubFactory(ApplicantApprovalFactory,
+        application_round=factory.SelfAttribute('..round'),
+    )
+    project = factory.SubFactory(ProjectFactory,
+        project_round__participating_round=factory.SelfAttribute('...round'),
+    )
+
+    @factory.post_generation
+    def mentors(self, create, extracted, **kwargs):
+        # create a mentor unless mentors=0 is explicitly specified
+        if extracted is None:
+            extracted = 1
+
+        if not create:
+            return
+
+        # allow mentors=3 to generate 3 new mentors
+        for _ in range(extracted):
+            MentorRelationshipFactory.create(
+                intern_selection=self,
+                mentor__project=self.project,
+                **kwargs
+            )
+
+class MentorRelationshipFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.MentorRelationship
+        django_get_or_create = ('intern_selection', 'mentor')
+
+    intern_selection = factory.SubFactory(InternSelectionFactory)
+    mentor = factory.SubFactory(MentorApprovalFactory)
+    contract = factory.SubFactory(SignedContractFactory)
