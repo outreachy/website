ef195a70fc2346829a97cc09a5df5451a65e885a
Jamey Sharp <jamey@minilop.net>
2018-12-14 16:21:26 -0800

    Fix server-side validation for RadioBooleanField
    Tested using the defyFormValidation extension for Firefox, which I'm so
    happy to have found:
    
    https://addons.mozilla.org/en-US/firefox/addon/defyformvalidation/

--- home/forms.py
+++ home/forms.py
@@ -1,8 +1,17 @@
 from django import forms
+from django.core.exceptions import ValidationError
 from django.forms import widgets
 
 
-class RadioBooleanField(forms.BooleanField):
+class RadioBooleanField(forms.NullBooleanField):
+    """
+    This is a form field for boolean-valued data where the desired UI is a
+    select-style widget instead of a checkbox. Unlike the stock BooleanField,
+    this means a correctly filled form absolutely must have data for this
+    field. So this style of field is equivalent to NullBooleanField, but with
+    the "Null" part removed: the field is always required and raises a
+    validation error if the input is None.
+    """
     widget = widgets.RadioSelect(choices=((True, 'Yes'), (False, 'No')))
 
     def __init__(self, **kwargs):
