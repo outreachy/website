6fc9f27f08d3da62c9e20b3a40cff6c8ac34c3a6
Sage Sharp <sharp@otter.technology>
2019-01-01 18:52:51 -0800

    Add a failing test to see if mentors can see hidden projects.
    For some reason this test is failing, even though when I reproduce it in
    my local browser with an older round and older mentored project, it
    looks fine. Could be a newly introduced bug, or it could be an issue
    with how the test case is set up.

--- home/test_roundpage.py
+++ home/test_roundpage.py
@@ -30,6 +30,62 @@ class RoundPageTestCase(TestCase):
         # Make sure that the contents don't include the approved project title from last round
         self.assertNotContains(response, project_title, status_code=200)
 
+    # XXX: FIXME - this test case looks fine when I recreate it in my local database
+    # and log in as a mentor with a pending project.
+    # However, the test case fails, showing both pending and approved project.
+    # It also shows "Project details are hidden.The details may be hidden because\nyou haven\'t completed an initial application."
+    # I suspect something is wrong with setting up the projects or mentors,
+    # but I don't know what.
+    @unittest.expectedFailure
+    def test_mentor_sees_hidden_projects(self):
+        # Before the Outreachy application period opens,
+        # We allow mentors with an approved project in an approved community
+        # to see all other approved projects (even if it's not in their community).
+        # Mentors with pending projects should only see their own project.
+        future = datetime.now(timezone.utc) - timedelta(days=10)
+        pending_project_title = "AAAAAAAAHHHHHHH! This project is pending!!"
+        pending_mentor_approval = MentorApprovalFactory(
+                approval_status=models.ApprovalStatus.APPROVED,
+                project__approval_status=models.ApprovalStatus.PENDING,
+                project__project_round__approval_status=models.ApprovalStatus.APPROVED,
+                project__short_title=pending_project_title,
+                project__project_round__participating_round__start_from='appsopen',
+                project__project_round__participating_round__start_date=future)
+
+        # Make a different mentor with an approved project under the same community
+        approved_project_title = "AAAAAAAAHHHHHHH! This project is approved!!"
+        approved_mentor_approval = MentorApprovalFactory(
+                approval_status=models.ApprovalStatus.APPROVED,
+                project__approval_status=models.ApprovalStatus.APPROVED,
+                project__project_round=pending_mentor_approval.project.project_round,
+                project__short_title=approved_project_title)
+
+        # Login as the pending mentor
+        self.client.force_login(pending_mentor_approval.mentor.account)
+
+        response = self.client.post(reverse('project-selection'))
+        # Make sure that the contents does include the mentor's pending project title
+        self.assertContains(response, '<h2>Your Pending Outreachy Internship Projects</h2>', status_code=200)
+        self.assertContains(response, pending_project_title, status_code=200)
+        # Make sure that the contents does not include the other mentor's approved project title
+        #print(response.content)
+        self.assertNotContains(response, approved_project_title, status_code=200)
+
+        # Login as the approved mentor
+        self.client.force_login(pending_mentor_approval.mentor.account)
+
+        response = self.client.post(reverse('project-selection'))
+        # Make sure that the contents does include the mentor's approved project title
+        self.assertContains(response, approved_project_title, status_code=200)
+        # Make sure that the contents does not include the other mentor's pending project title
+        self.assertNotContains(response, pending_project_title, status_code=200)
+
+        # TODO:
+        # Set the pending project to approved and make sure both mentors can see both projects
+        # Make a mentor who is approved under a different community
+        # Make sure that mentor can see all three projects
+        # Make sure all three mentors can see all projects on all community landing pages
+
     def test_application_round_open(self):
         # Make a round where the application period is open
         # Make an approved project in an approved community
