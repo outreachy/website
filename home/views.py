5be83d70316459c02b667c550e63926ef6c3968c
Jamey Sharp <jamey@minilop.net>
2019-01-11 22:51:18 -0800

    Remove unused mentored_projects variable
    Even if it were used, I'm pretty sure it has exactly the same projects
    as the mentors_pending_projects list computed immediately before it.

--- home/views.py
+++ home/views.py
@@ -1026,17 +1026,14 @@ def community_landing_view(request, round_slug, community_slug):
     if request.user.is_authenticated:
         try:
             mentors_pending_projects = participation_info.mentors_pending_projects(request.user.comrade)
-            mentored_projects = [p for p in projects if p.is_submitter(request.user)]
 
             approved_coordinator = participation_info.is_approved_coordinator(request.user)
         # Even though the user is authenticated, they may not have a Comrade
         except Comrade.DoesNotExist:
             mentors_pending_projects = None
-            mentored_projects = None
             approved_coordinator = False
     else:
         mentors_pending_projects = None
-        mentored_projects = None
         approved_coordinator = False
 
     return render(request, 'home/community_landing.html',
