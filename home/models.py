outreachy/website

commit abd08668d0b0892d956bc287389c14dc404df089

New get_deadline_date_for utility function

home/models.py
@@ -104,12 +104,37 @@ class BlogIndex(RoutablePageMixin, Page):
 # the given date at 4PM UTC.
 DEADLINE_TIME = datetime.time(hour=16, tzinfo=datetime.timezone.utc)
 
+def get_deadline_date_for(dt):
+    """
+    Takes a timezone-aware datetime and returns the date which is
+    comparable to dates in RoundPage deadlines. If the datetime has
+    not reached the deadline time of 4PM UTC, then this is the
+    previous day's date.
+
+    This is handy for comparing an arbitrary point in time against
+    any of the deadlines in RoundPage, like the following example to
+    find rounds where the intern announcement deadline has passed but
+    the internship end deadline has not:
+
+    >>> import datetime
+    >>> now = datetime.datetime.now(datetime.timezone.utc)
+    >>> today = get_deadline_date_for(now)
+    >>> RoundPage.objects.filter(
+    ...     internannounce__lte=today,
+    ...     internends__gt=today,
+    ... )
+    <PageQuerySet [...]>
+    """
+    if dt.timetz() < DEADLINE_TIME:
+        return dt.date() - datetime.timedelta(days=1)
+    return dt.date()
+
 def has_deadline_passed(deadline_date):
     if not deadline_date:
         return False
-    deadline = datetime.datetime.combine(deadline_date, DEADLINE_TIME)
-    now = datetime.datetime.now(deadline.tzinfo)
-    return deadline < now
+    now = datetime.datetime.now(DEADLINE_TIME.tzinfo)
+    today = get_deadline_date_for(now)
+    return deadline_date < today
 
 class RoundPage(Page):
     roundnumber = models.IntegerField()

commit 369b66e203f8778cbd2036cb0bea897e425bea3d

Deadline has passed on the deadline date too
We've reached a given (4PM UTC) deadline once it is _equal to_ "now", so
`has_deadline_passed` needs to check if the deadline is less than _or
equal_ to today.

Before I broke this in abd08668d0b0892d956bc287389c14dc404df089, this
function was comparing datetimes with sub-second resolution, so missing
the equality check was only wrong for a fraction of a second. Now that
the resolution is 24 hours instead, this is wrong for... 24 hours. And
that's too many hours.

home/models.py
@@ -134,7 +134,7 @@ def has_deadline_passed(deadline_date):
         return False
     now = datetime.datetime.now(DEADLINE_TIME.tzinfo)
     today = get_deadline_date_for(now)
-    return deadline_date < today
+    return deadline_date <= today
 
 class RoundPage(Page):
     roundnumber = models.IntegerField()