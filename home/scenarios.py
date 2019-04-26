aea6ee775ffc6f827b966277de964d8b6c051bfa
Sage Sharp <sharp@otter.technology>
2019-04-26 15:13:43 -0700

    Scenarios: WIP scenario for internship selection period.
    This creates three projects under the same community, with approved
    mentors. It creates three applicants who have been selected as interns
    by the mentors. The funding and organizer approval are in various states
    for the interns.
    
    It still needs to create Contribution and FinalApplication objects for
    the applicants.

--- home/scenarios.py
+++ home/scenarios.py
@@ -6,6 +6,7 @@ up complete scenarios which are useful for various testing purposes.
 import factory
 from . import factories
 from home.models import ApprovalStatus
+from home.models import InternSelection
 
 class Scenario(object):
     def __init__(self, **kwargs):
@@ -83,4 +84,103 @@ class ApplicationsOpenScenario(NewRoundScenario):
         mentor=factory.SelfAttribute('..mentor'),
         project=factory.SelfAttribute('..project'),
         approval_status=ApprovalStatus.APPROVED,
+    )
+
+class InternSelectionScenario(ApplicationsOpenScenario):
+    """
+    Create samples of the objects which matter once applicants are allowed to
+    begin applying to participate in the new round.
+    """
+
+    round__start_from = 'mentor_intern_selection_deadline'
+    sponsorship__amount = 13000
+
+    mentor2 = factory.SubFactory(factories.MentorFactory)
+    mentor3 = factory.SubFactory(factories.MentorFactory)
+
+    applicant1 = factory.SubFactory(
+        factories.ApplicantApprovalFactory,
+        applicant=factory.SubFactory(factories.ApplicantFactory),
+        application_round=factory.SelfAttribute('..round'),
+        approval_status=ApprovalStatus.APPROVED,
+    )
+    applicant2 = factory.SubFactory(
+        factories.ApplicantApprovalFactory,
+        application_round=factory.SelfAttribute('..round'),
+        applicant=factory.SubFactory(factories.ApplicantFactory),
+        approval_status=ApprovalStatus.APPROVED,
+    )
+    applicant3 = factory.SubFactory(
+        factories.ApplicantApprovalFactory,
+        application_round=factory.SelfAttribute('..round'),
+        applicant=factory.SubFactory(factories.ApplicantFactory),
+        approval_status=ApprovalStatus.APPROVED,
+    )
+
+    project2 = factory.SubFactory(
+        factories.ProjectFactory,
+        project_round=factory.SelfAttribute('..participation'),
+        approval_status=ApprovalStatus.APPROVED,
+    )
+    project3 = factory.SubFactory(
+        factories.ProjectFactory,
+        project_round=factory.SelfAttribute('..participation'),
+        approval_status=ApprovalStatus.APPROVED,
+    )
+
+    mentor_approval2 = factory.SubFactory(
+        factories.MentorApprovalFactory,
+        mentor=factory.SelfAttribute('..mentor2'),
+        project=factory.SelfAttribute('..project2'),
+        approval_status=ApprovalStatus.APPROVED,
+    )
+    mentor_approval3 = factory.SubFactory(
+        factories.MentorApprovalFactory,
+        mentor=factory.SelfAttribute('..mentor3'),
+        project=factory.SelfAttribute('..project3'),
+        approval_status=ApprovalStatus.APPROVED,
+    )
+
+    intern_selection1 = factory.SubFactory(
+        factories.InternSelectionFactory,
+        round=factory.SelfAttribute('..round'),
+        applicant=factory.SelfAttribute('..applicant1'),
+        project=factory.SelfAttribute('..project'),
+        funding_source=InternSelection.UNDECIDED_FUNDING,
+        organizer_approved=None,
+        mentors=0,
+    )
+    intern_selection2 = factory.SubFactory(
+        factories.InternSelectionFactory,
+        round=factory.SelfAttribute('..round'),
+        applicant=factory.SelfAttribute('..applicant2'),
+        project=factory.SelfAttribute('..project2'),
+        funding_source=InternSelection.ORG_FUNDED,
+        organizer_approved=True,
+        mentors=0,
+    )
+    intern_selection3 = factory.SubFactory(
+        factories.InternSelectionFactory,
+        round=factory.SelfAttribute('..round'),
+        applicant=factory.SelfAttribute('..applicant3'),
+        project=factory.SelfAttribute('..project3'),
+        funding_source=InternSelection.GENERAL_FUNDED,
+        organizer_approved=False,
+        mentors=0,
+    )
+
+    mentor_relationship1 = factory.SubFactory(
+        factories.MentorRelationshipFactory,
+        intern_selection=factory.SelfAttribute('..intern_selection1'),
+        mentor=factory.SelfAttribute('..mentor_approval'),
+    )
+    mentor_relationship2 = factory.SubFactory(
+        factories.MentorRelationshipFactory,
+        intern_selection=factory.SelfAttribute('..intern_selection2'),
+        mentor=factory.SelfAttribute('..mentor_approval2'),
+    )
+    mentor_relationship3 = factory.SubFactory(
+        factories.MentorRelationshipFactory,
+        intern_selection=factory.SelfAttribute('..intern_selection3'),
+        mentor=factory.SelfAttribute('..mentor_approval3'),
     )
