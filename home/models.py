4096670f13ba84018b522bdc8a7624ae000184fb
Sage Sharp <sharp@otter.technology>
2019-05-27 08:34:28 -0500

    Rename chats & descriptions, add new chat urls and dates.
    Last round, Outreachy interns mentioned it would be good to have more
    focused chats. This round, instead of having interns just kind of "drop
    in" their blog post links into the chat channel, we're going to have
    more focused community chats about the blog post topics. They'll take
    place at a specific time, so this commit adds chat dates and chat urls
    for all chats.
    
    This commit also renames the chat field names and verbose name to
    reflect the date the chat actually happens. We send out the blog post
    prompt, and then a week later have a chat about the topic.

--- home/models.py
+++ home/models.py
@@ -155,9 +155,16 @@ class RoundPage(Page):
     internstarts = models.DateField("Date internships start", default='2017-12-05')
     week_two_chat_text_date = models.DateTimeField("Date and time of outreachy week two chat (text only)", default='2018-12-10 13:00Z')
     week_two_chat_text_url = models.URLField(blank=True, verbose_name="URL of the real-time text chat")
-    week_three_stuck_chat_url = models.URLField(blank=True, verbose_name="URL of the week three chat on what we're stuck on")
-    week_five_audience_chat_url = models.URLField(blank=True, verbose_name="URL of the week five chat to explain your project to a newcomer")
-    week_seven_timeline_chat_url = models.URLField(blank=True, verbose_name="URL of the week seven chat to talk about project timeline modifications")
+    week_four_chat_text_date = models.DateTimeField("Date and time of Outreachy week four chat about what we're stuck on", default='2018-12-10 13:00Z')
+    week_four_stuck_chat_url = models.URLField(blank=True, verbose_name="URL of the week four chat on what we're stuck on")
+    week_six_chat_text_date = models.DateTimeField("Date and time of Outreachy week six chat to explain why your project passion to a newcomer", default='2018-12-10 13:00Z')
+    week_six_audience_chat_url = models.URLField(blank=True, verbose_name="URL of the week six chat to explain your project to a newcomer")
+    week_eight_chat_text_date = models.DateTimeField("Date and time of Outreachy week eight chat to talk about difficulties scoping project tasks", default='2018-12-10 13:00Z')
+    week_eight_timeline_chat_url = models.URLField(blank=True, verbose_name="URL of the week eight chat to talk about project timeline modifications")
+    week_ten_chat_text_date = models.DateTimeField("Date and time of Outreachy week ten chat to talk about career opportunities", default='2018-12-10 13:00Z')
+    week_ten_career_chat_url = models.URLField(blank=True, verbose_name="URL of the week ten chat to talk about career opportunities")
+    week_twelve_chat_text_date = models.DateTimeField("Date and time of Outreachy week twelve chat to wrap up the Outreachy internship", default='2018-12-10 13:00Z')
+    week_twelve_wrapup_chat_url = models.URLField(blank=True, verbose_name="URL of the week twelve chat to wrap up the Outreachy internship")
     initialfeedback = models.DateField("Date initial feedback is due", blank=True, default='2017-12-20')
     initialpayment = models.IntegerField(default=1000)
     midfeedback = models.DateField("Date mid-point feedback is due", blank=True, default='2018-01-31')
@@ -193,9 +200,16 @@ class RoundPage(Page):
         FieldPanel('finalpayment'),
         FieldPanel('week_two_chat_text_date'),
         FieldPanel('week_two_chat_text_url'),
-        FieldPanel('week_three_stuck_chat_url'),
-        FieldPanel('week_five_audience_chat_url'),
-        FieldPanel('week_seven_timeline_chat_url'),
+        FieldPanel('week_four_chat_text_date'),
+        FieldPanel('week_four_stuck_chat_url'),
+        FieldPanel('week_six_chat_text_date'),
+        FieldPanel('week_six_audience_chat_url'),
+        FieldPanel('week_eight_chat_text_date'),
+        FieldPanel('week_eight_timeline_chat_url'),
+        FieldPanel('week_ten_chat_text_date'),
+        FieldPanel('week_ten_career_chat_url'),
+        FieldPanel('week_twelve_chat_text_date'),
+        FieldPanel('week_twelve_wrapup_chat_url'),
     ]
 
     def official_name(self):
