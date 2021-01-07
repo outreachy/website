09a2530a5460deaa0fa110445f585f97f161cc65
Sage Sharp <sage@sfconservancy.org>
2021-01-07 10:24:31 -0800

    Blog: Add December 2020 blog post to the blog index page and RSS feed

--- home/feeds.py
+++ home/feeds.py
@@ -103,6 +103,13 @@ class WagtailFeed(Feed):
             first_published_at=pacific.localize(datetime.datetime(2020, 3, 27, 16, 00, 0)),
             last_published_at=pacific.localize(datetime.datetime(2020, 5, 1, 16, 00, 0)),
         ))
+        items.append(PseudoPage(
+            title='December 2020 internship applications open',
+            full_url=reverse('2020-08-initial-apps-open'),
+            owner=author,
+            first_published_at=pacific.localize(datetime.datetime(2020, 8, 28, 9, 00, 0)),
+            last_published_at=pacific.localize(datetime.datetime(2020, 8, 28, 9, 00, 0)),
+        ))
 
         # put the Wagtail pages and special posts together in the right order
         items.sort(key=operator.attrgetter('first_published_at'), reverse=True)
